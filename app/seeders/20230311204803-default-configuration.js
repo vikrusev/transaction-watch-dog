ef533d99b86d30c322f1199419ad2f56f6406c8a
Viktor Rusev <vikrusev257@gmail.com>
2023-03-11 23:45:17 +0200

    add initial configuration seed

---
+++ app/seeders/20230311204803-default-configuration.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { readFileSync } = require('fs');
+const path = require('path');
+
+const dataFilePath = path.join(__dirname, 'default-configuration.json');
+const data = JSON.parse(readFileSync(dataFilePath, 'utf8'));
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Configurations', data.configuration);
+    await queryInterface.bulkInsert('ConfigurationVersions', data.configurationVersion)
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('ConfigurationVersions', null, {});
+    await queryInterface.bulkDelete('Configurations', null, {});
+  }
+};
