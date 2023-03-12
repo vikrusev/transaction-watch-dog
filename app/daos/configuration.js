9056bad26a67cabb61bb1969045e8df6d9b81567
Viktor Rusev <vikrusev257@gmail.com>
2023-03-12 18:46:29 +0200

    managed Sequelize transactions & Configuration creation

--- app/daos/configuration.js
+++ app/daos/configuration.js
@@ -1,4 +1,5 @@
-const { Configuration } = require('../models');
+const { Configuration, ConfigurationVersion } = require('../models');
+const sequelize = require('../db');
 
 /**
  * DAO for a Configuration
@@ -6,9 +7,27 @@ const { Configuration } = require('../models');
 class ConfigurationDao {
     constructor() { }
 
-    async create(configuration) {
-        const result = await Configuration.create(configuration)
-        return result.id;
+    /**
+     * Creates a new Configuration entry in the DB
+     * Adds a record in the Configurations and ConfigurationVersions tables
+     * @param {*} data - 
+     * @returns 
+     */
+    async create(data) {
+        const newConfigurationId = await sequelize.transaction(async t => {
+            const newConfiguration = await Configuration.create({
+                active: data.active
+            });
+            await ConfigurationVersion.create({
+                ...data,
+                versionNumber: 1,
+                configId: newConfiguration.id
+            });
+
+            return newConfiguration.id;
+        })
+
+        return newConfigurationId;
     }
 }
 
--- app/db/index.js
+++ app/db/index.js
@@ -1,4 +1,8 @@
+const cls = require('cls-hooked');
+const namespace = cls.createNamespace('my-very-own-namespace');
+
 const { Sequelize } = require('sequelize');
+Sequelize.useCLS(namespace)
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
