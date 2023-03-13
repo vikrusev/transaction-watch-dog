4e74f3c45466288c2a2fbb592b3c47265ea66bb5
Viktor Rusev <vikrusev257@gmail.com>
2023-03-13 12:28:37 +0200

    accumulate transactions and send to worker_threads

--- app/services/event-emitter.js
+++ app/services/event-emitter.js
@@ -6,19 +6,30 @@ const functions = require('../web3/transaction-utilities')
 
 const WorkerPool = require('../worker-pool');
 
+const transactionsData = [];
+
 // Add an event listener
-eventEmitter.on('new-transaction', async (transactionData) => {
+eventEmitter.on('new-transaction', (transactionData) => {
+    transactionsData.push(transactionData)
+});
+
+// Accumulate transactions for X amount of milliseconds
+// Then provide all to a worker thread
+setInterval(async () => {
+    const copiedTransactions = transactionsData.map(transaction => {return {...transaction}})
+
     // check if the workerpool is enabled from the environment
     if (process.env.WORKER_POOL_ENABLED === '1') {
         // execute a function through the workerPool middlware proxy
         const workerPoolProxy = WorkerPool.getProxy()
-        const result = await workerPoolProxy.filterTransactions(transactionData, ActiveConfiguration.getActiveConfigurationList())
+        const result = await workerPoolProxy.filterTransactions(copiedTransactions, ActiveConfiguration.getActiveConfigurationList())
     } else { // TODO check
-        setInterval(async () => {
-            const result = await functions.filterTransactions(transactionData)
-        }, 5000)
+        const result = await functions.filterTransactions(copiedTransactions)
     }
-});
+
+    // reset the accumulative transactions array
+    transactionsData.length = 0
+}, 5000)
 
 // Export the event emitter as a module
 module.exports = eventEmitter;
--- app/web3/transaction-utilities.js
+++ app/web3/transaction-utilities.js
@@ -1,5 +1,14 @@
-const filterTransactions = (transaction, activeConfigurationList) => {
-    return activeConfigurationList.length
+/**
+ * Filter transactions which should be saved in the DB
+ * The worker_thread should not insert in the DB directly
+ * @param {*} transactionsData - a chunk of transactions to be filtered by a worker_thread
+ * @param {*} activeConfigurationList - a list of the current active Configurations
+ * @returns the transactions to be saved in the DB from the mainThread
+ */
+const filterTransactions = (transactionsData, activeConfigurationList) => {
+    const result = transactionsData.length
+
+    return result;
 }
 
 module.exports = {
