cb5a65be91e27fb27db2f1ddfbb71b7cfefd4afe
Viktor Rusev <vikrusev257@gmail.com>
2023-03-13 15:47:56 +0200

    add id to createTransactionConfigVersion migration

--- app/migrations/20230311191543-create-transaction-config-version.js
+++ app/migrations/20230311191543-create-transaction-config-version.js
@@ -3,6 +3,12 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('TransactionConfigVersions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
       transactionId: {
         allowNull: false,
         type: Sequelize.INTEGER,
