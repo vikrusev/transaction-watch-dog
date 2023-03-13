6f005aa4268f1a018336250a498042c533b492c3
Viktor Rusev <vikrusev257@gmail.com>
2023-03-13 15:48:17 +0200

    bulk insert transactionConfigVersion relations

--- app/daos/transaction.js
+++ app/daos/transaction.js
@@ -13,8 +13,41 @@ class TransactionDao {
 
         return await sequelize.transaction(async t => {
             const newTransactions = await Transaction.bulkCreate(transactionsData);
+
+            const flatMapRelations = this.flatMapNewTransactionIdToConfigurationVersion(newTransactions, entries)
+            await TransactionConfigVersion.bulkCreate(flatMapRelations);
+
             console.log(`Successfully added ${newTransactions.length} transactions!`)
         })
+    }
+
+    /**
+     * The idea is to make this: [ { id: 5, array: [1,2,3] }, { id: 6, array: [4,5,6] } ];
+     * Into this [ { id: 5, val: 1 },
+                    { id: 5, val: 2 },
+                    { id: 5, val: 3 },
+                    { id: 6, val: 4 },
+                    { id: 6, val: 5 },
+                    { id: 6, val: 6 } ]
+     * @param {*} newTransactions 
+     * @param {*} entries 
+     * @returns the proper format for inserting relations with bulkCreate
+     */
+    flatMapNewTransactionIdToConfigurationVersion(newTransactions, entries) {
+        const relations = newTransactions.map((transaction) => {
+            const entry = entries[transaction.hash]
+            return {
+                transactionId: transaction.id,
+                configurationVersionIds: entry.configurationIds
+            }
+        })
+
+        return relations.flatMap((relation) =>
+            relation.configurationVersionIds.map((configVersionId) => ({
+                transactionId: relation.transactionId,
+                configVersionId
+            }))
+        )
     }
 }
 
